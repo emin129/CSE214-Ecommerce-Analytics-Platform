b5b8f16ad840cdf2595c473af834aa4449c23757
mehmet emin ejderha <mehmetemin3819@gmail.com>
2026-04-12 14:30:00 +0300

    feat: setup spring boot backend architecture and dependencies

---
+++ backend/src/main/java/com/ecommerce/ecommerce_backen/EcommerceBackenApplication.java
@@ -0,0 +1,16 @@
+package com.ecommerce.ecommerce_backen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EcommerceBackenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackenApplication.class, args);
+    }
+
+    
+}
+
